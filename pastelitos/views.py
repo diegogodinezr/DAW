8dd0baf65591b5427ea131ef900af18625877dd0
diegogodinezr <godinezroblesdiego@gmail.com>
2023-03-01 20:13:14 -0600

    1 mar

--- pastelitos/views.py
+++ pastelitos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import *
 from .models import *
+from django.shortcuts import redirect
 
 # Create your views here.
 def landing(request):
@@ -13,3 +14,38 @@ def landing(request):
         'consulta': consulta,
     }
     return render (request,template_to_return,context)
+def post_pastel(request):
+    if request.method=="POST":
+        form=pastelform(request.POST,request.FILES)
+        if form.is_valid():
+            print(form)
+            pastel.objects.create(
+                cubierta = request.POST["cubierta"],
+                precio = request.POST["precio"],
+                sabor = request.POST["sabor"],
+                peso = request.POST["peso"],
+                pisos = request.POST["pisos"],
+                tipo = request.POST["tipo"],
+            )
+            return redirect('landing')
+        else:
+            form = pastelform()
+        return render(request,'hola.htl',context)
+    
+def updatepastel (request, id):
+    resultado = pastel.objects.get(id = id)
+    print (id)
+    id=id
+    form = pastelform(instance=resultado)
+    template_to_return = "uptadepastel.html"
+    if request.method=="POST":
+        print("hola")
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form': form,
+        'resultado': resultado,
+        'id':id,
+    }
+    return render (request, template_to_return, context)
