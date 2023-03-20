029d12f695df89444b3df9079ddbc665a8b4686a
diegogodinezr <godinezroblesdiego@gmail.com>
2023-03-20 09:42:33 -0600

    a_productos

--- abarrotes/urls.py
+++ abarrotes/urls.py
@@ -7,7 +7,12 @@ from .views import *
 urlpatterns = [
     url(r'^$',landinga, name="landinga"),
 
-    url('landingproductoa/',landingproductoa, name="landingproductoaa"),
+    url('landingproductoa/',landingproductoa, name="landingproductoa"),
+    path('post_productoa/', post_productoa, name="post_productoa"),
+    path('detalle_productoa/<int:id>', updateproductoa, name="updateproductoa"),
+    path('postm_productoa/<int:id>', updateproductoa, name="postm_productoa"),
+    path('sacar_datos_productoa/<int:id>', updateproductoa,name="postm_productoa"),
+    path('removerproductoa', removerproductoa, name='removerproductoa'),
 
     url('landingclientea/',landingclientea, name="landingclientea"),
 
--- abarrotes/views.py
+++ abarrotes/views.py
@@ -26,6 +26,59 @@ def landingproductoa(request):
         'consulta': consulta,
     }
     return render (request,template_to_return,context)
+ 
+def post_productoa(request):
+    if request.method=="POST":
+        form=productoform(request.POST,request.FILES)
+        if form.is_valid():
+            producto.objects.create(
+                nombre = request.POST["nombre"],
+                precio = request.POST["precio"],
+                categoria = request.POST["categoria"],
+                cantidad = request.POST["cantidad"],
+                tipo = request.POST["tipo"],
+            )
+            return redirect('landingproductoa')
+        else:
+            form = productoform()
+        return render(request,'producto.html')
+    
+def updateproductoa(request,id):
+    resultado=producto.objects.get(id=id)
+    id=id
+    form=productoform(instance=resultado)
+    template_to_return = "updateproducto.html"
+    if request.method=="POST":
+        form=productoform(request.POST,request.FILES)
+        if form.is_valid():
+            resultado.nombre = request.POST["nombre"]
+            resultado.precio = request.POST["precio"]
+            resultado.categoria = request.POST["categoria"]
+            resultado.cantidad = request.POST["cantidad"]
+            resultado.tipo = request.POST["tipo"]
+            resultado.save()
+            return redirect("landingproductoa")
+
+    context={
+        'form':form,
+        'resultado':resultado,
+        'id':id,
+    }
+    return render (request, template_to_return,context)
+
+def sacar_datos_productoa(request, id):
+    objeto = producto.objects.get(id=id)
+    id=id
+    formulario = productoform(instance=objeto)
+    datos = {'formulario': formulario}
+    html_form = render_to_string('updateproducto.html', datos, request=request)
+    return HttpResponse(html_form,id)
+
+def removerproductoa(request):
+    idproducto = request.POST["idproducto"]
+    consulta = producto.objects.get(id=idproducto)
+    consulta.delete()
+    return HttpResponse('producto eliminado correctamente')
 
 #=================CLIENTE====================
 def landingclientea(request):
